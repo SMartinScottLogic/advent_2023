2f7d6a144809b9b7f8eba99d505393a14f1fbe03
Simon Martin <smartin@scottlogic.com>
2023-12-04 13:20:46 +0000

    Day 5 init

---
+++ day5/src/lib.rs
@@ -0,0 +1,49 @@
+use std::io::{BufRead, BufReader};
+
+pub type ResultType = u64;
+
+#[derive(Debug, Default)]
+pub struct Solution {}
+
+impl utils::Solution for Solution {
+    type Result = anyhow::Result<ResultType>;
+    fn analyse(&mut self, _is_full: bool) {}
+
+    fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        // Implement for problem
+        Ok(0)
+    }
+
+    fn answer_part2(&self, _is_full: bool) -> Self::Result {
+        // Implement for problem
+        Ok(0)
+    }
+}
+
+#[allow(unused_variables, unused_mut)]
+impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
+    type Error = std::io::Error;
+
+    fn try_from(reader: BufReader<T>) -> Result<Self, Self::Error> {
+        let mut solution = Self::default();
+        for line in reader.lines().flatten() {
+            // Implement for problem
+        }
+        Ok(solution)
+    }
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::BufReader;
+
+    use utils::Solution;
+
+    #[test]
+    fn read() {
+        let input = "replace for problem";
+        let r = BufReader::new(input.as_bytes());
+        let s = crate::Solution::try_from(r).unwrap();
+        assert_eq!(0 as ResultType, s.answer_part1(false).unwrap());
+    }
+}
