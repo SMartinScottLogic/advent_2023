719250084c4d59e9276787733ffe946f1d3ff928
Simon Martin <smartin@scottlogic.com>
2023-12-05 05:43:51 +0000

    Day 5 part 1 complete

--- day5/src/lib.rs
+++ day5/src/lib.rs
@@ -1,17 +1,48 @@
-use std::io::{BufRead, BufReader};
+use std::{io::{BufRead, BufReader}, collections::HashMap};
+
+use tracing::info;
 
 pub type ResultType = u64;
 
+#[derive(Debug)]
+struct RangeMap {
+    source_start: ResultType,
+    dest_start: ResultType,
+    range_length: ResultType,
+}
+impl RangeMap {
+    fn includes(&self, seed: ResultType) -> bool {
+        seed >= self.source_start && seed < self.source_start + self.range_length
+    }
+    fn convert(&self, seed: ResultType) -> ResultType {
+        let delta = self.dest_start as i64 - self.source_start as i64;
+        (seed as i64 + delta).try_into().unwrap()
+    }
+}
 #[derive(Debug, Default)]
-pub struct Solution {}
+pub struct Solution {
+    seeds: Vec<ResultType>,
+    maps: HashMap<String, Vec<RangeMap>>,
+}
 
 impl utils::Solution for Solution {
     type Result = anyhow::Result<ResultType>;
     fn analyse(&mut self, _is_full: bool) {}
 
     fn answer_part1(&self, _is_full: bool) -> Self::Result {
+        let result = self.seeds.iter().map(|seed| {
+            let soil = self.map("seed-to-soil", *seed);
+            let fertilizer = self.map("soil-to-fertilizer", soil);
+            let water = self.map("fertilizer-to-water", fertilizer);
+            let light = self.map("water-to-light", water);
+            let temperature = self.map("light-to-temperature", light);
+            let humidity = self.map("temperature-to-humidity", temperature);
+            let location = self.map("humidity-to-location", humidity);
+            info!(seed, soil, fertilizer, water, light, temperature, humidity, location, "seed");
+            location
+        }).min().unwrap();
         // Implement for problem
-        Ok(0)
+        Ok(result)
     }
 
     fn answer_part2(&self, _is_full: bool) -> Self::Result {
@@ -20,14 +51,106 @@ impl utils::Solution for Solution {
     }
 }
 
+impl Solution {
+    fn set_seeds(&mut self, seeds: Vec<ResultType>) {
+        self.seeds = seeds;
+    }
+    fn add_map(&mut self, name: &str, source_start: ResultType, dest_start: ResultType, range_length: ResultType) {
+        let map = self.maps.entry(name.to_owned()).or_insert_with(Vec::new);
+        map.push(RangeMap {source_start, dest_start, range_length});
+    }
+    fn map(&self, name: &str, source: ResultType) -> ResultType {
+        let maps = self.maps.get(name).unwrap();
+        for map in maps {
+            if map.includes(source) {
+                return map.convert(source);
+            }
+        }
+        return source;
+    }
+}
+
 #[allow(unused_variables, unused_mut)]
 impl<T: std::io::Read> TryFrom<BufReader<T>> for Solution {
     type Error = std::io::Error;
 
     fn try_from(reader: BufReader<T>) -> Result<Self, Self::Error> {
+        let r = regex::Regex::new(r"^(?<dest_start>\d+)\s+(?<source_start>\d+)\s+(?<range_len>\d+)$").unwrap();
+
         let mut solution = Self::default();
+        let mut stage = 0;
+        let mut has_header = false;
         for line in reader.lines().flatten() {
+            if line.trim().is_empty() {
+                stage += 1;
+                has_header = false;
+                continue;
+            }
             // Implement for problem
+            match stage {
+                // seeds
+                0 => {
+                    let (_, seeds) = line.split_once(": ").unwrap();
+                    let seeds = seeds.split(" ").map(|v| v.parse().unwrap()).collect();
+                    solution.set_seeds(seeds);
+                }
+                _ if !has_header => {
+                    has_header = true;
+                }
+                1 => {
+                    let c = r.captures(&line).unwrap();
+                    let dest_start = c.name("dest_start").unwrap().as_str().parse().unwrap();
+                    let source_start = c.name("source_start").unwrap().as_str().parse().unwrap();
+                    let range_length = c.name("range_len").unwrap().as_str().parse().unwrap();
+                    solution.add_map("seed-to-soil", source_start, dest_start, range_length);
+                }
+                2 => {
+                    let c = r.captures(&line).unwrap();
+                    let dest_start = c.name("dest_start").unwrap().as_str().parse().unwrap();
+                    let source_start = c.name("source_start").unwrap().as_str().parse().unwrap();
+                    let range_length = c.name("range_len").unwrap().as_str().parse().unwrap();
+                    solution.add_map("soil-to-fertilizer", source_start, dest_start, range_length);
+                }
+                3 => {
+                    let c = r.captures(&line).unwrap();
+                    let dest_start = c.name("dest_start").unwrap().as_str().parse().unwrap();
+                    let source_start = c.name("source_start").unwrap().as_str().parse().unwrap();
+                    let range_length = c.name("range_len").unwrap().as_str().parse().unwrap();
+                    solution.add_map("fertilizer-to-water", source_start, dest_start, range_length);
+                }
+                4 => {
+                    let c = r.captures(&line).unwrap();
+                    let dest_start = c.name("dest_start").unwrap().as_str().parse().unwrap();
+                    let source_start = c.name("source_start").unwrap().as_str().parse().unwrap();
+                    let range_length = c.name("range_len").unwrap().as_str().parse().unwrap();
+                    solution.add_map("water-to-light", source_start, dest_start, range_length);
+                }
+                5 => {
+                    let c = r.captures(&line).unwrap();
+                    let dest_start = c.name("dest_start").unwrap().as_str().parse().unwrap();
+                    let source_start = c.name("source_start").unwrap().as_str().parse().unwrap();
+                    let range_length = c.name("range_len").unwrap().as_str().parse().unwrap();
+                    solution.add_map("light-to-temperature", source_start, dest_start, range_length);
+                }
+                6 => {
+                    let c = r.captures(&line).unwrap();
+                    let dest_start = c.name("dest_start").unwrap().as_str().parse().unwrap();
+                    let source_start = c.name("source_start").unwrap().as_str().parse().unwrap();
+                    let range_length = c.name("range_len").unwrap().as_str().parse().unwrap();
+                    solution.add_map("temperature-to-humidity", source_start, dest_start, range_length);
+                }
+                7 => {
+                    let c = r.captures(&line).unwrap();
+                    let dest_start = c.name("dest_start").unwrap().as_str().parse().unwrap();
+                    let source_start = c.name("source_start").unwrap().as_str().parse().unwrap();
+                    let range_length = c.name("range_len").unwrap().as_str().parse().unwrap();
+                    solution.add_map("humidity-to-location", source_start, dest_start, range_length);
+                }
+                _ => panic!("unknown parse stage {stage}: {line}")
+            }
+            if stage == 0 {
+
+            }
         }
         Ok(solution)
     }
